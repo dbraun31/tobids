5187edadb4c6fc7d3d1c078caf7bfdb56585256c
Dave Braun <dave.braun@drexel.edu>
2023-10-10 16:09:58 -0400

    Update notes

--- eeg-to-bids.py
+++ eeg-to-bids.py
@@ -1,4 +1,5 @@
 import mne
+import scipy.io as scp
 
 file = 'sample_data/MW_EEG_02_00.vhdr'
 
@@ -9,3 +10,5 @@ eeg = mne.io.read_raw_brainvision(file, preload=False, verbose='error')
 montage = eeg.get_montage()
 montage.get_positions()
 
+## gotta figure out how this works
+data = scp.loadmat('sample_data/MW_EEG_002_01_12-Jul-2023_11_11.mat')
