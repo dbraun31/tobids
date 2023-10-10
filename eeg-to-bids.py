3b5e180d03abc5a39e1f8aa03c99b5bdef3c0b8c
Dave Braun <dave.braun@drexel.edu>
2023-10-10 15:44:59 -0400

    Update notes on documentation

--- eeg-to-bids.py
+++ eeg-to-bids.py
@@ -1,10 +1,10 @@
 import mne
 
-file = 'MW_EEG_02_00.vhdr'
+file = 'sample_data/MW_EEG_02_00.vhdr'
 
 
 # mne website said to keep preload false...
-eeg = mne.io.read_raw_brainvision(file, preload=True, verbose='error')
+eeg = mne.io.read_raw_brainvision(file, preload=False, verbose='error')
 
 montage = eeg.get_montage()
 montage.get_positions()
